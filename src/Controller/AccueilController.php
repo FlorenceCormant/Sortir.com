a2a57c14b56e15692daf13f87e9ede2beee131a0
Julliot <bjulliot35@gmail.com>
2021-05-31 16:47:12 +0200

    Ajout des fichier CSS (Normalize et style.css) Import de boostrap NavBar Affichage de base des sorties

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Sorties;
+use App\Repository\SortiesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +15,14 @@ class AccueilController extends AbstractController
     /**
      * @Route("/", name="accueil_home")
      */
-    public function home(): Response
+    public function home(EntityManagerInterface $entityManager, SortiesRepository $sortiesRepository): Response
     {
-        return $this->render('accueil/home.html.twig', [
+        $sortiesRepository = $this->getDoctrine()->getRepository(Sorties::class);
+        $sortiesRepository = $entityManager->getRepository(Sorties::class);
+        $sorties = $sortiesRepository->findAll();
 
+        return $this->render('accueil/home.html.twig', [
+            "sorties" => $sorties
         ]);
     }
 }
